c15eaf3d9b3c65015e21fb49b905a024243f2681
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2022-12-06 20:55:03 +0530

    Revert "created car_sales_price_prediction.jl"
    This reverts commit 32efb6f43ff0b912b637365b88d31cdb175387a7.

--- car_sales_price_prediction.jl
+++ car_sales_price_prediction.jl
@@ -1,4 +1,15 @@
-# Selling price predictions
 using CSV, DataFrames, MLJ, Plots, TypedTables
 
+cars = CSV.read("data/CAR DETAILS FROM CAR DEKHO.csv",DataFrame)
+# cars
+
+# describe(cars)
+
+# x = cars.year
+# y = cars.selling_price
+# plot(x,y)
+
+# models(matching(x,y))
+
+# plots_heatmap(cars.selling_price)
 
