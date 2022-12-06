dcaa0c880cf84c2c80c75a602ae0b44694050f7b
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2022-12-06 22:52:19 +0530

    fixed some issues

--- Notebook01.jl
+++ Notebook01.jl
@@ -1,2 +1,6 @@
-using CSV, DataFrames, GLM, Plots, TypedTables
-CSV.read("data/CAR DETAILS FROM CAR DEKHO.csv",DataFrame)
+using DataFrames, CSV, GLM, Plots, TypedTables
+# cars = CSV.File("CAR DETAILS FROM CAR DEKHO.csv")
+cars = CSV.read("CAR DETAILS FROM CAR DEKHO.csv",DataFrame)
+describe(cars)
+x = cars.year
+y = cars.selling_price
