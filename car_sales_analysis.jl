284281846173a0ce512fb45526b3f0ef6ae18abf
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2022-12-07 02:35:00 +0530

    made changes

--- car_sales_analysis.jl
+++ car_sales_analysis.jl
@@ -3,3 +3,6 @@ cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
 # Relationship b/w years and selling prices
 years_and_selling_prices = scatter(cars.year,cars.selling_price)
 Plots.pdf(years_and_selling_prices, "Output/pdf/years_and_selling_prices")
+# Relationship b/w years and km drivens
+years_and_km_driven = scatter(cars.year,cars.km_driven)
+Plots.pdf(years_and_km_driven, "Output/pdf/years_and_km_driven")
