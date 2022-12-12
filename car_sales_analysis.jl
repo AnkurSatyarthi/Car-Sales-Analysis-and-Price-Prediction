91c8216e32a2d778c96c282e3d8984cdc3b27f9b
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2022-12-12 11:31:20 +0530

    nothing

--- car_sales_analysis.jl
+++ car_sales_analysis.jl
@@ -2,7 +2,8 @@ using CSV, GLM, Plots, TypedTables
 cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
 # Relationship b/w years and selling prices
 years_and_selling_prices = scatter(cars.year,cars.selling_price)
-Plots.pdf(years_and_selling_prices, "Output/pdf/years_and_selling_prices")
 # Relationship b/w years and km drivens
 years_and_km_driven = scatter(cars.year,cars.km_driven)
-Plots.pdf(years_and_km_driven, "Output/pdf/years_and_km_driven")
+x = cars.year
+y = cars.selling_price
+
