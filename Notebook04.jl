daafb94c145e91fc732e8335e760ad4e6816faf4
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-03 18:54:54 +0530

    nothing special

--- Notebook04.jl
+++
@@ -1,8 +0,0 @@
-using DelimitedFiles, GR, Plots, DataFrames, CSV, Makie, PyPlot, GLM, MLJ
-DataFrame()
-DataFrame(A=1:19,B=5:13,fixed=1)
-DataFrame("employee"=>["Ronny","Mike","Michale","Robin","Mini"],
-            "occupation"=>["Security Guard","Supervisor","Owner","Security Guard","Marketing Relationships"],
-            "salary"=>[9000,12000,25000,8500,12000],
-            "Bonus"=>[0,500,2000,500,500],
-            "Day"=[" "," "," "," "," "])
