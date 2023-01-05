84d105dad587681d2676595bb9955fb01a040c31
Ankur <ankurindiaofficial@gmail.com>
2023-01-05 17:38:56 +0530

    .

--- Car analysis.jl
+++ Car analysis.jl
@@ -1,24 +1,28 @@
 using DataFrames, CSV, GLM, Plots, TypedTables, StatsPlots
 cars = DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
-cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
-# Relationship b/w years and selling prices
-years_and_selling_prices = scatter(cars.year,cars.selling_price)
-# Relationship b/w years and km drivens
-years_and_km_driven = scatter(cars.year,cars.km_driven)
-x = cars.year
-y = cars.selling_price
+# cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
 describe(cars)
-X = cars.km_driven
-Y = cars.selling_price
-vscodedisplay(cars)
-plot(X,Y)
-# plot(x,Y,resolution=(2000,2000))
+first(cars,5)
+last(cars,5)
+year = cars.year
+selling_price = cars.selling_price
+km_driven = cars.km_driven
+# Relationship b/w years and selling prices
+# years_and_selling_prices = scatter(cars.year,cars.selling_price)
+# Relationship b/w years and km drivens
+# years_and_km_driven = @df cars scatter(x=cars.km_driven,
+                                        # :km_driven,
+                                        # :selling_price,
+                                        # y=cars.selling_price,
+                                        # ms=5,
+                                        # ma=0.5,
+                                        # title="Relationship between km driven and selling prices",
+                                        # xlabel="km driven",
+                                        # ylabel="selling_price")
+# vscodedisplay(cars)
 scatter(x,Y,ms=5,ma=0.5,title="Relationship between km driven and selling prices",lw=1,xlabel="km driven",ylabel="selling price")
-a=:x
-b = :Y
-@df cars plot(:Y)
-@df cars scatter(x,Y)
-df = DataFrame(a=1:100,b=100*rand(100),c=100*rand(100))
-@df df plot(:a, [:b, :c])
-@df df scatter(:a,:b,title="plot title")
-@df cars corrplot(cols(2:3),grid=false)
+@df cars plot(:selling_price)
+# kmyrsprp=@df cars corrplot(cols(2:4),grid=false)
+kmyrsprp=@df cars corrplot(cols(3:4),grid=false,title="selling_price and ")
+save("km_driven,selling_price, and rp.svg",kmyrsprp)
+save("km_driven,selling_price, and rp.pdf",kmyrsprp)
