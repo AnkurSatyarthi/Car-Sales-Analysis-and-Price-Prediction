d85d71e966cbb368b2de9cd8bf98b1670678df17
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-12 23:58:19 +0530

    Made some changes

--- car_sales_price_prediction.jl
+++ car_sales_price_prediction.jl
@@ -17,8 +17,8 @@ Y = round.(Int, cars.selling_price / 1000)
 t = Table(X = X, Y = Y)
 gr(size = (600, 600))
 plot_scatter = scatter(X, Y,
-    xlims = (0, 810),
-    ylims = (0, 8900),
+    xlims = (0, 850),
+    ylims = (0, 8999),
     xlabel = "KM DRIVEN (In thousands)",
     ylabel = "PRICE (In thousands of dollars)",
     title = "Car selling prices",
@@ -27,7 +27,7 @@ plot_scatter = scatter(X, Y,
 )
 ols = lm(@formula(Y ~ X), t)
 plot!(X, predict(ols), color = :green, linewidth = 3)
-new_X = Table(X = [159])
+new_X = Table(X = [277])
 predict(ols, new_X)
 epochs = 0
 plot_scatter = scatter(X, Y,
@@ -57,15 +57,17 @@ end
 function pd_theta_1(X,Y)
     (1/m)*sum((y_hat-Y).*X)
 end
-alpha_0=0.09
-alpha_1=0.00000008
-theta_0_temp = pd_theta_0(X,Y)
-theta_1_temp = pd_theta_1(X,Y)
-theta_0 -= alpha_0*theta_0_temp
-theta_1 -= alpha_1*theta_1_temp
-y_hat=h(X)
-J=cost(X,Y)
-push!(J_history, J)
-epochs+=1
-plot!(X,y_hat,color=:red,alpha=0.5,
-    title="Car Selling Prices (epochs = $epochs")
+# alpha_0=0.01
+# alpha_1=0.01
+
+# theta_0_temp = pd_theta_0(X,Y)
+# theta_1_temp = pd_theta_1(X,Y)
+# theta_0 -= alpha_0*theta_0_temp
+# theta_1 -= alpha_1*theta_1_temp
+# y_hat=h(X)
+# J=cost(X,Y)
+
+# push!(J_history, J)
+# epochs+=1
+# plot!(X,y_hat,color=:red,alpha=0.5,
+#     title="Car Selling Prices (epochs = $epochs")
