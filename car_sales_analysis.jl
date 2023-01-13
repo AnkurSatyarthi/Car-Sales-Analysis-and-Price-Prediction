277df5c6ac51827058f08891cf0f17f193214662
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-13 11:37:13 +0530

    Minor changes

--- car_sales_analysis.jl
+++ car_sales_analysis.jl
@@ -4,7 +4,8 @@ cars = DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
 # Relationship b/w years and selling prices
 years_and_selling_prices = scatter(cars.year,cars.selling_price)
 # Relationship b/w years and km drivens
-years_and_km_driven = scatter(cars.year,cars.km_driven)
+
+
 x = cars.year
 y = cars.selling_price
 
