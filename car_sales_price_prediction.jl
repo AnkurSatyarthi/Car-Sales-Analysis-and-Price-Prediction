b0cfcccbe1aaa734d24d8fd6d06fc69245ac05d6
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-13 10:42:21 +0530

    added car years prediction

--- car_sales_price_prediction.jl
+++ car_sales_price_prediction.jl
@@ -1,9 +1,9 @@
 using DataFrames, CSV, GLM, Plots, TypedTables
 cars = DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
 describe(cars)
-X = round.(Int, cars.km_driven / 1000)
+X = round.(Int,cars.km_driven/1000)
 # describe(X)
-Y = round.(Int, cars.selling_price / 1000)
+Y = round.(Int,cars.selling_price/1000)
 # describe(Y)
 # years_and_km_driven = @df cars scatter(X,
 #                                         # :km_driven,
@@ -43,20 +43,20 @@ theta_0 = 0.0
 theta_1 = 0.0
 h(x) = theta_0 .+ theta_1 * x
 plot!(X, h(X), color = :blue, linewidth = 3)
-m = length(X)
-y_hat = h(X)
-function cost(X,Y)
-    (1/(2*m))*sum((y_hat-Y).^2)
-end
-J=cost(X,Y)
-J_history=[]
-push!(J_history, J)
-function pd_theta_0(X,Y)
-    (1/m)*sum(y_hat-Y)
-end
-function pd_theta_1(X,Y)
-    (1/m)*sum((y_hat-Y).*X)
-end
+# m = length(X)
+# y_hat = h(X)
+# function cost(X,Y)
+#     (1/(2*m))*sum((y_hat-Y).^2)
+# end
+# J=cost(X,Y)
+# J_history=[]
+# push!(J_history, J)
+# function pd_theta_0(X,Y)
+#     (1/m)*sum(y_hat-Y)
+# end
+# function pd_theta_1(X,Y)
+#     (1/m)*sum((y_hat-Y).*X)
+# end
 # alpha_0=0.01
 # alpha_1=0.01
 
