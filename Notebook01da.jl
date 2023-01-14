81bba65461a4e8334ccf9ecfbc8441a9ffa102dc
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-14 11:09:18 +0530

    Minor changes

--- Notebook01da.jl
+++
@@ -1,1 +0,0 @@
-# using VegaDatasets, DataVoyager, VegaLite
--- car_sales_analysis.jl
+++ car_sales_analysis.jl
@@ -2,13 +2,5 @@ using CSV, GLM, Plots, TypedTables
 cars = DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
 # cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
 # Relationship b/w years and selling prices
-years_and_selling_prices = scatter(cars.year,cars.selling_price)
+years_and_selling_prices = scatter()
 # Relationship b/w years and km drivens
-
-
-x = cars.year
-y = cars.selling_price
-
-describe(cars)
-X = cars.km_driven
-Y = cars.selling_price
