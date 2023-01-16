fc54b5817d5bf14cc97af055caec4d6941791bfb
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-16 10:07:31 +0530

    Made some minor changes

--- car_sales_analysis.jl
+++
@@ -1,6 +0,0 @@
-using CSV, GLM, Plots, TypedTables
-cars = DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
-# cars = CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv")
-# Relationship b/w years and selling prices
-years_and_selling_prices = scatter()
-# Relationship b/w years and km drivens
