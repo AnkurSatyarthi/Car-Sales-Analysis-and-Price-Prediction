7ceb71427e9ec797dfbbcd9f140e3a01d0748349
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-18 22:59:17 +0530

    Made some minor changes

--- untitled.jl
+++ untitled.jl
@@ -1,4 +1,4 @@
-using DataFrames, CSV, GLM, Plots, CategoricalArrays, TypedTables, StatsPlots
+using DataFrames, CSV, GLM, Plots, CategoricalArrays, TypedTables, StatsPlots, CairoMakie, GLMakie, Makie.Colors
 cars=DataFrame(CSV.File("data/CAR DETAILS FROM CAR DEKHO.csv"))
 describe(cars)
 X1=round.(Int,cars.km_driven/1000); X2=cars.year; Y=round.(Int,cars.selling_price/1000);
@@ -14,7 +14,7 @@ X1=round.(Int,cars.km_driven/1000); X2=cars.year; Y=round.(Int,cars.selling_pric
 #                                         ylabel = "PRICE (In thousands of Rupees, ref: CarDekho)",
 #                                         legend = false,
 #                                         color = :skyblue)
-t=Table(X1=X1, X2=X2, Y=Y); gr(size=(600,600))
+t=Table(X1=X1, X2=X2, Y=Y); gr(size=(600,600));
 plot_scatter = scatter(X1, X2, Y,
     ma=0.5,
     ms=5,
@@ -29,3 +29,5 @@ plot_scatter = scatter(X1, X2, Y,
     color = :teal
 )
 ols=lm(@formula(Y ~ X1),t)
+fig=Figure();ax1=Axis(fig[1,1]);ax2=Axis(fig[1,2]);ax3=Axis(fig[2,1:2]);fig;lines!(ax1,X1);lines!(ax2,X2);lines!(ax3,Y);fig
+fig=Figure();ax1,l1=lines(fig[1,1],0..850,X1,color=:red);ax2,l2=lines(fig[2,1],1992..2022,X2,color=:blue);Legend(fig[1:2,2],[l1,l2],["km_driven","year"]);fig
